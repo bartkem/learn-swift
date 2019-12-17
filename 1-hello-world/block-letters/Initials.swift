28134a2ec3243dd15db17ccec03ae305d23ebec9
bartkem <58977058+bartkem@users.noreply.github.com>
2019-12-17 11:51:22 +0100

    Update Initials.swift

--- 1-hello-world/block-letters/Initials.swift
+++ 1-hello-world/block-letters/Initials.swift
@@ -8,3 +8,17 @@ print("  SSS   L     ")
 print("     S  L     ")
 print(" S   S  L     ")
 print("  SSS   LLLLL ")
+
+
+
+// Bart Kem
+//FUN COMMENT I Have second son (he has month) and try to learn every day when he goes to slep :)  
+print("""
+      BBBB    K   K     
+      B   B   K  K
+      B   B   K K    
+      BBBB    KK 
+      B   B   K K   
+      B   B   K  K
+      BBBB    K   K
+      """)
